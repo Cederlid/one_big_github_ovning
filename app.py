1f6815f225dc692cc6f1cc90dec67ac2ab518853
Wijdan Cederlid <hardrock_17@hotmail.com>
2025-08-17 21:59:56 +0200

    added flask and updated requirements

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
-from dotenv import load_dotenv
-import os
-import numpy as np
+# from flask import Flask
+from flask import Flask
 
-load_dotenv()
-def random_number_generator():
-    secret = os.getenv('SUPER_SECRET_PHRASE')
-    expected_secret = os.getenv('EXPECTED_SECRET')
+app = Flask(__name__)
 
-    if secret and expected_secret and secret == expected_secret:
-        print(np.random.randint(1, 101))
-    else:
-        print("Access denied: invalid or missing secret phrase.")
+@app.route('/')
+def hello():
+    return "Hello from Docker!"
 
-if __name__ == "__main__":
-    random_number_generator()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
